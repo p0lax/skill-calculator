1299858e97e233894b76242df9efa6c4f3ceac6f
p0lax <polax@list.ru>
2023-10-29 16:20:31 +0100

    Removed example. Fixed background

--- src/features/Calculator/Calculator.tsx
+++ src/features/Calculator/Calculator.tsx
@@ -1,11 +1,10 @@
-import styles from "./Calculator.module.css";
-import { CalculatorBody } from "./CalculatorBody/CalculatorBody";
-import { CalculatorHeader } from "./CalculatorHeader/CalculatorHeader";
+import styles from './Calculator.module.css';
+import { CalculatorBody } from './CalculatorBody/CalculatorBody';
+import { CalculatorHeader } from './CalculatorHeader/CalculatorHeader';
 
 export const Calculator = () => {
   return (
     <div className={styles.root}>
-      {/* <div className={styles.example}></div> */}
       <CalculatorHeader />
       <CalculatorBody />
     </div>
